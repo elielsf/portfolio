347ef3c4509b44f740298f014b3ad36f49a0e0ed
elielsf <elielasf@hotmail.com>
2022-12-27 21:07:56 -0300

    CSS Adicionado, estilização de home e about pronta

--- portifolio/src/components/body/Body.tsx
+++ portifolio/src/components/body/Body.tsx
@@ -1,51 +1,66 @@
 import React from "react";
 import styles from './Body.module.css';
+import Perfil from '../../assets/images/perfil.png'
+import Insta from '../../assets/images/instagram.png'
+import Whats from '../../assets/images/whatsapp.png'
+import Linkedin from '../../assets/images/linkedin.png'
+import Email from '../../assets/images/email.png'
+
 
 
 
 export default function Body() {
     return(
         <div className={styles.container}>
-            <div className={styles.topo}>
-                <img src="" alt="" className={styles.insta} />
-                <img src="" alt="" className={styles.whatsApp} />
-                <img src="" alt="" className={styles.linkedin} />
-                <img src="" alt="" className={styles.email} />
-            </div>
             <div className={styles.inicio}>
                 <h1 className={styles.tituloInicio}>HOME</h1>
                 <div className={styles.corpoInicio}>
-                    <img src="" alt="" className={styles.logo} />
-                    <p className={styles.introducao}>Eliel Silva, <br></br>junior front-end developer.</p>
+                    <div className={styles.textoInicio}>
+                    <h2 className={styles.introducao}>Front-end<br></br> developer.</h2>
+                        <div className={styles.redesSociais}>
+                            <img src={Linkedin} alt="linkedin" className={styles.linkedin} />
+                            <img src={Email} alt="mail" className={styles.email} />
+                            <img src={Whats} alt="zapzap" className={styles.whatsApp} />
+                            <img src={Insta} alt="insta" className={styles.insta} />
+                        </div>
+                    </div>
+                    <img src={Perfil} alt="" className={styles.perfil} />
                 </div>
             </div>
 
             <div className={styles.sobre}>
                 <h1 className={styles.tituloSobre}>ABOUT ME</h1>
-                <div className={styles.corpoSobre}>
-                    <h2>Português-Br</h2>
+                <div className={styles.corpoPt}>
+                    <h2>Portuguese</h2>
                     <p className={styles.textoPt}>
                         <b>Olá,</b> <br></br> 
-                        meu nome é Eliel, tenho 19 anos e sou um desenvolvedor junior.
-                        No momento estou fazendo Psicologia, é, eu sei que não tem relação alguma mas não escolhi amar duas coisas distintas, rs.
-                        Estou no primeiro ano de psicologia, como bolsista, estou aprendendo a programar nas horas vagas e acredito que a psicologia possa contribuir bastante no meu relacionamento dentro do
-                        trabalho!!!
-                        Tive meu primeiro contato com a programação no colegial, em uma espécie de "técnico" de exatas agregado ao ensino médio, voltei a ter contato em 2022 e me apaixonei.
-                        Hoje estou cursando desenvolvimento web full stack pela Newtab Academy.
-                        Após terminar a etapa de front end do curso, estou em busca de emprego na área e apesar de não ter experiência trabalhando de maneira remunerada, o curso é imersivo e a gente vive algo muito próximo do trabalho real, realizando metodologias ágeis como o scrum, trabalhos em equipe, projetos que atendem a demanda do mercado, tem prazos de entrega e notas de qualidade.
-                        Estou muito ansioso pra arrumar o meu primeiro emprego! Não sei tudo, mas quero muito um dia chegar perto de saber!
+                        meu nome é Eliel, tenho 19 anos e sou um desenvolvedor junior. <br></br>
+                        No momento estou fazendo Psicologia, é, eu sei que não tem relação alguma mas não escolhi amar duas coisas distintas, rs.<br></br>
+                        Estou no primeiro ano de psicologia, como bolsista, estou aprendendo a programar nas horas vagas e acredito que a psicologia
+                        possa contribuir bastante no meu relacionamento dentro do trabalho!!!<br></br>
+                        Tive meu primeiro contato com a programação no colegial, em uma espécie de "técnico" de exatas agregado ao ensino médio, 
+                        voltei a ter contato em 2022 e me apaixonei. Hoje estou cursando desenvolvimento web full stack pela Newtab Academy.<br></br>
+                        Após terminar a etapa de front end do curso, estou em busca de emprego na área e apesar de não ter experiência trabalhando
+                        de maneira remunerada, o curso é imersivo e a gente vive algo muito próximo do trabalho real, realizando metodologias ágeis
+                        como o scrum, trabalhos em equipe, projetos que atendem a demanda do mercado, tem prazos de entrega e notas de qualidade.<br></br>
+                        Estou muito ansioso pra arrumar o meu primeiro emprego! Não sei tudo, mas quero muito um dia chegar perto de saber!<br></br>
                         Quer contar com alguém com fome de aprendizado? continue rolando para baixo.
                     </p>
-                    <h2>English</h2>
+                </div>
+                <div className={styles.corpoIngles}>
+                <h2>English</h2>
                     <p className={styles.textoEng}>
                         <b>Hello,</b> <br></br> 
-                        my name is Eliel, I'm 19 years old and I'm a junior developer. 
-                        At the moment I'm doing psychology, yeah, I know it's unrelated, but I didn't choose to love two different things. 
-                        I am in my first year of psychology, as a scholarship student, I am learning to program in my spare time and I believe that psychology can contribute a lot to my relationship at work.
+                        my name is Eliel, I'm 19 years old and I'm a junior developer.<br></br>
+                        At the moment I'm doing psychology, yeah, I know it's unrelated, but I didn't choose to love two different things.<br></br> 
+                        I am in my first year of psychology, as a scholarship student, I am learning to program in my spare time and I believe that psychology<br></br>
+                        can contribute a lot to my relationship at work.<br></br>
                         I had my first contact with programming in high school, in a kind of "technical" high school, and I fell in love with it again in 2022.
-                        Today I am studying full stack web development at Newtab Academy.
-                        After finishing the front end stage of the course, I am looking for a job in the area and although I have no experience working in a paid way, the course is immersive and we live something very close to the real work, doing agile methodologies like scrum, team work, projects that meet the market demand, have deadlines and quality grades.
-                        I am very eager to get my first job! I don't know everything, but I really want to get close to knowing one day. 
+                        Today I am studying full stack web development at Newtab Academy.<br></br>
+                        After finishing the front end stage of the course, I am looking for a job in the area and although I have no experience working in a paid way, the course 
+                        is immersive and we live something very close to the real work, doing agile methodologies like scrum, team work, projects that meet the market demand, 
+                        have deadlines and quality grades.<br></br>
+                        I am very eager to get my first job! I don't know everything, but I really want to get close to knowing one day.<br></br>
                         Do you want someone with a hunger for learning? keep scrolling down.
                     </p>
                 </div>
--- portifolio/src/components/header/Header.tsx
+++ portifolio/src/components/header/Header.tsx
@@ -1,11 +1,12 @@
 import styles from './Header.module.css';
+import Logo from '../../assets/images/logo.png'
 
 export default function Header() {
     return (
         <div className={styles.cabecalho}>
             <button className={styles.botaoInicio}>Home</button>
             <button className={styles.botaoSobre}>About me</button>
-            <img src="" alt="" className={styles.logoHeader} />
+            <img src={Logo} alt="" className={styles.logoHeader} />
             <button className={styles.botaoProjetos}>Projects</button>
             <button className={styles.botaoHabilidades}>Skills</button>
         </div>
